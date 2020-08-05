a22b3a6fb89e344e3ae59d879b7689e59ce6dd5e
zachgitt <zachgitt@umich.edu>
2020-08-05 12:27:42 -0400

    completed exploring to patrolling

--- src/main.py
+++ src/main.py
@@ -16,24 +16,52 @@ class FiniteStateMachine:
 			3: 'manipulating',
 			4: 'dumping'
 		}
+		self.exploring_completed = False
+		self.exploring_subscribed = False
 		
 	def get_state(self):
 		s = self.switcher.get(self.state, 'Invalid State')
 		return String(s)
 
-	def next(self):
-		# Explore only once
-		if self.state == 0:
-			# TODO: move from explore to patrol, read explore is comleted
-			pass
+	def mark_completed(self, data):
+		print('CALLBACK-----------------------------------------')
+		if data.data == 'completed':
+			print('COMPLETED-----------------------------------------')
+			self.exploring_completed = True
 
-		# Go from dumping to patrolling
-		if self.state + 1 == self.num_states:
-			self.state = 1
+	def next(self):
+		# Explore only once, then go to patrolling
+		if self.state == 0:
+			
+			print('STATE0 ------------------------------------------------')
+			# Initialize subscriber
+			if self.exploring_subscribed == False:
+				rospy.Subscriber('exploring_completed', String, self.mark_completed)
+			        print('SUBSCRIBED--------- ------------------------------------------------')
+				self.exploring_subscribed = True
+
+			# Launch turtlebot3_navigation, as part of next state		
+			if self.exploring_completed:
+				# TODO: change path to catkin_ws
+			        print('DONE EXPLORING--------- ------------------------------------------------')
+				path = '/home/zacharygittelman/work_all/src/turtlebot3_navigation/launch/turtlebot3_navigation_orch.launch'
+				uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+				roslaunch.configure_logging(uuid)
+				launch = roslaunch.parent.ROSLaunchParent(uuid, [path]) 
+				launch.start()
+				rospy.loginfo('navigation launched')
+
+				# Increment state
+				self.state += 1
 
 		# Go to next state 
+		elif self.state + 1 < self.num_states:
+			self.state += 1 
+
+		# Restart the cycle excluding exploring, go from dumping to patrolling
 		else:
-			self.state = (self.state + 1) % self.num_states
+			self.state = 1
+
 
 
 def orchestrator():
@@ -42,6 +70,7 @@ def orchestrator():
 	rospy.init_node('orch_node')
 	pub = rospy.Publisher('state', String, queue_size=10)
 	r = rospy.Rate(0.2) # 5hz
+	
 
 	# Initialize finite state machine
 	fsm = FiniteStateMachine()
@@ -52,7 +81,6 @@ def orchestrator():
 		# Publish state
 		pub.publish(fsm.get_state())
 
-		# TODO: Add logic to update state properly
 		# Update state
 		fsm.next()
 
