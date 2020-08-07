5e47bd8b648bac0692f9a04ad6c410b4ddc42b05
zachgitt <zachgitt@umich.edu>
2020-08-07 17:30:33 -0400

    updating launch to include april tag in world

--- src/main.py
+++ src/main.py
@@ -4,54 +4,64 @@ import rospy
 import roslaunch
 from std_msgs.msg import String 
 
+# Define globals
+workspace_path = '/home/zacharygittelman/work_all' # TODO: use catkin_workspace or current workspace, maybe a parameter
+EXPLORING_COMPLETED = False
+TRASHCAN_DETECTED = False
+
+
+def exploring_callback(data):
+	print('CALLBACK-----------------------------------------')
+	if data.data == 'completed':
+		print('COMPLETED-----------------------------------------')
+		global EXPLORING_COMPLETED
+		EXPLORING_COMPLETED = True
+
+def trashcan_callback(data):
+	if data.data == 1: # TODO: potentially change 1 to True
+		global TRASHCAN_DETECTED
+		TRASHCAN_DETECTED = True
+
+
 class FiniteStateMachine:
 
 	def __init__(self):
 		self.state = 0
-		self.num_states = 5
+		self.num_states = 6
 		self.switcher = {
 			0: 'exploring',
 			1: 'patrolling',
-			2: 'approaching',
-			3: 'manipulating',
-			4: 'dumping'
+			2: 'approach_trash',
+			3: 'pickup_trash',
+			4: 'approach_trashcan',
+      			5: 'dumping_trash'
 		}
-		self.exploring_completed = False
-		self.exploring_subscribed = False
 		
 	def get_state(self):
 		s = self.switcher.get(self.state, 'Invalid State')
 		return String(s)
 
-	def mark_completed(self, data):
-		print('CALLBACK-----------------------------------------')
-		if data.data == 'completed':
-			print('COMPLETED-----------------------------------------')
-			self.exploring_completed = True
-
 	def next(self):
+
 		# Explore only once, then go to patrolling
 		if self.state == 0:
 			
 			print('STATE0 ------------------------------------------------')
-			# Initialize subscriber
-			if self.exploring_subscribed == False:
-				rospy.Subscriber('exploring_completed', String, self.mark_completed)
-			        print('SUBSCRIBED--------- ------------------------------------------------')
-				self.exploring_subscribed = True
 
-			# Launch next state 
-			if self.exploring_completed:
-				# TODO: change path to catkin_ws
+			# Move to Patrolling 
+			if EXPLORING_COMPLETED:
+
 			        print('DONE EXPLORING--------- ------------------------------------------------')
-				path = '/home/zacharygittelman/work_all/src/turtlebot3_navigation/launch/turtlebot3_navigation_orch.launch'
+
+				# Launch navigation
+				path = workspace_path + '/src/turtlebot3_navigation/launch/turtlebot3_navigation_orch.launch'
 				uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 				roslaunch.configure_logging(uuid)
 				launch = roslaunch.parent.ROSLaunchParent(uuid, [path]) 
 				launch.start()
 				rospy.loginfo('navigation launched')
 
-				# Launch patrol file
+				# Run patrol file
 				package = 'patrol'
 				executable = 'random_nav_orch.py'
 				node = roslaunch.core.Node(package, executable)
@@ -62,6 +72,56 @@ class FiniteStateMachine:
 
 				# Increment state
 				self.state += 1
+
+		# Patrolling
+		elif self.state == 1:
+
+			# THIS IS ALL DONE IN orch LAUNCH FILE	
+			# Launch apriltag
+			# roslaunch apriltag_ros continuous_detection.launch
+			# Run trashcan tracking
+			# rosrun object_tracking detect_trashcan.py
+			# rosrun object_tracking find_trashcan.py
+
+			# Start looking for water bottle
+			if TRASHCAN_DETECTED:	
+				pass			
+
+				# TODO: run find_trash.py
+				# TODO: add qrcode to turtlebot3_gazebo turtlebot3_world.launch
+
+				# Run 2 below: look for water bottle
+				# roslaunch darknet_ros darknet_ros.launch
+				# rosrun object_tracking find_trash.py
+
+			# Switch to approaching trash
+			# if trashcan_detected and /trash_detected: True:
+				# switch to state2=approaching_trash
+
+		# Approach trash
+		elif self.state == 2:
+			pass			
+			# subscribe /trash_approached: True
+				# switch to state3=pickup_trash
+
+		# Pickup trash
+		elif self.state == 3:
+			pass			
+			# subscribe /trash_picked_up: True 
+				# switch to state4=approaching_trashcan
+
+		# Approaching trashcan
+		elif self.state == 4:
+			pass			
+			# subscribe /trashcan_approached: True
+				# switch to state5=dumping_trash
+
+		# Dumping trash
+		else:
+			pass			
+			# subscribe /trash_dumped: True
+				# switch to state1=patrolling	
+		
 
 		## Go to next state 
 		#elif self.state + 1 < self.num_states:
@@ -74,6 +134,9 @@ class FiniteStateMachine:
 
 
 def orchestrator():
+
+	# Initialize subscribers
+	rospy.Subscriber('exploring_completed', String, exploring_callback)
 
 	# Initialize publisher
 	rospy.init_node('orch_node')
