a8e63a65737ac3eccfea615c845c5b5f305aa0c8
Zwee Dao <zweedao@gmail.com>
2020-08-23 14:02:47 -0400

    fix

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -4,8 +4,9 @@ from rospy.numpy_msg import numpy_msg
 from std_msgs.msg import Float32, Float32MultiArray, Bool, String
 import time
 
+TRASHCAN_DETECTED = False
 TRASH_DETECTED = False
-#TRASHCAN_DETECTED = False
+TRASH_PICKUP_DONE = False
 
 def state_callback(msg):
     global TRASH_DETECTED
@@ -19,21 +20,27 @@ def state_callback(msg):
     if msg.data == 'trashcan_detected':
         TRASHCAN_DETECTED = True
 
-    if msg.data == 'trash_detected' and TRASHCAN_DETECTED:
+    if msg.data == 'trash_detected':
+        TRASH_DETECTED = True
+
+    if TRASHCAN_DETECTED and TRASH_DETECTED and not TRASH_PICKUP_DONE:
         state_pub.publish('approach_trash')
 
     if msg.data = 'approach_trash_done':
         state_pub.publish('pickup_trash')
 
     if msg.data = 'pickup_trash_done':
+        TRASH_PICKUP_DONE = True
         state_pub.publish('approach_trashcan')
 
     if msg.data = 'approach_trashcan_done':
         state_pub.publish('drop_trash')
 
     if msg.data = 'drop_trash_done':
+        print('Trash dropped!')
+        TRASH_DETECTED = False
+        TRASH_PICKUP_DONE = False
         #state_pub.publish('patrol')
-        print('Done!')
 
 
 def state_listener():
